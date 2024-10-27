92988a12b81393c87d795be904d3b67c40fe99fa
FaresNaoui1 <faresnaoui198gmail.com>
2024-10-28 00:17:52 +0100

    to push in render.com

--- database.py
+++ database.py
@@ -1,16 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from config import URL_DATABASE
 
-import os
-
-URL_DATABASE = os.getenv("DATABASE_URL")  # Set DATABASE_URL in Render
-
-
-# Set up database engine
-engine = create_engine(URL_DATABASE, connect_args={"charset": "utf8mb4"})
-
-# Create a configured "Session" class
+# Set up database engine and session
+engine = create_engine(URL_DATABASE)
 SessionLocal = sessionmaker(autoflush=False, bind=engine)
-
-# Create a Base class for declarative models
 Base = declarative_base()
