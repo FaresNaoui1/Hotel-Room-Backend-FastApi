341c6c95ffe00251d7f8a914c89ea4ac3e44f74f
FaresNaoui1 <faresnaoui198gmail.com>
2024-10-28 00:08:13 +0100

    to push in render.com

--- database.py
+++ database.py
@@ -1,12 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-# Database URL should be set up as an environment variable for security.
-# Example: export DATABASE_URL="mysql+pymysql://root:12345@localhost:3306/hotel"
-DATABASE_URL = "mysql+pymysql://root:12345@localhost:3306/hotel"  # Change this in production
+import os
+
+URL_DATABASE = os.getenv("DATABASE_URL")  # Set DATABASE_URL in Render
+
 
 # Set up database engine
-engine = create_engine(DATABASE_URL, connect_args={"charset": "utf8mb4"})
+engine = create_engine(URL_DATABASE, connect_args={"charset": "utf8mb4"})
 
 # Create a configured "Session" class
 SessionLocal = sessionmaker(autoflush=False, bind=engine)
